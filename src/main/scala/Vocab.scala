61d5f6e9ebb6d84c32ef0a8af48e6f11333d9126
Alexey Alekhin <laughedelic@gmail.com>
2017-02-28 02:09:06 +0100

    Added HanziDB to the links (only for single characters)

--- src/main/scala/Vocab.scala
+++ src/main/scala/Vocab.scala
@@ -10,22 +10,27 @@ case class Vocab(
   reading: Pinyin,
   toughness: Int,
   toughnessString: String,
+  ilk: String,
   audio: Option[String],
   definitions: Map[LangCode, String],
   dictionaryLinks: Map[String, String]
 ) {
 
+  val links: Map[String, String] = dictionaryLinks ++ Seq(
+    Some("大БКРС" -> s"https://bkrs.info/slovo.php?ch=${writing}"),
+    if (ilk != "char") None else
+      Some("HanziDB" -> s"http://hanzidb.org/lookup?q=${writing}")
+  ).flatten
+
   def markdown: String = {
     Seq(
-      s"${writing}  🔉${reading}  💪${toughness} (${toughnessString})",
+      s"${writing}  /🔉${reading}  💪${toughness} (${toughnessString})",
 
       definitions.map { case (lang, defn) =>
         s"""${flags.get(lang).getOrElse(lang + ":")} ${defn}"""
       }.mkString("\n"),
 
-      ( dictionaryLinks +
-        ("大БКРС" -> s"https://bkrs.info/slovo.php?ch=${writing}")
-      ).collect { case (name, url) if url.nonEmpty =>
+      links.collect { case (name, url) if url.nonEmpty =>
         s"[${name}](${url})"
       }.mkString(" | ")
 
